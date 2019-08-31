91e8101201bf88be4801c7098639d9b97e441e4f
yuzhangYuan <yuanyuzhangyyzz@163.com>
2019-08-31 18:53:37 +0800

    fix the bug of todos.filter

--- src/components/completeItem/View.js
+++ src/components/completeItem/View.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './style.css'
 import FunctionButtons from '../functionButtons'
 
-function View({ todos, filter }) {
+function View(props) {
 
-    let completeList = todos && todos.map((todo, index) => {
-        if ((todo.completed === true) && ((todo.filter === "SHOW_ALL") || (todo.filter === "SHOW_COMPLETED"))) {
+    let completeList = props.todos && props.todos.map((todo, index) => {
+        if ((todo.completed === true) && ((props.filter === "SHOW_ALL") || (props.filter === "SHOW_COMPLETED"))) {
             return (<li key={index} data-index={index}> {todo.text} <FunctionButtons /> </li>);
         }
         else {
@@ -13,8 +13,7 @@ function View({ todos, filter }) {
         }
     })
 
-    if ((filter === "SHOW_ALL") || (filter === "SHOW_COMPLETED")) {
-        console.log(filter)
+    if ((props.filter === "SHOW_ALL") || (props.filter === "SHOW_COMPLETED")) {
         return (
             <ul className="todo" id="completed">
                 {completeList}
