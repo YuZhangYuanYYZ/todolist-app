0b22a9274295597e0f336f4caf0ca60379e9cff8
Jing Liu <jing.liu@rea-group.com>
2019-10-26 22:38:16 +1100

    better format

--- src/components/RightSideBar/View.js
+++ src/components/RightSideBar/View.js
@@ -9,111 +9,85 @@ import './style.css'
 import "react-datepicker/dist/react-datepicker.css";
 
 class View extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: new Date()
+        };
+        this.onHandleChange = this.onHandleChange.bind(this);
+        this.changeDueDate = this.changeDueDate.bind(this);
+        this.dueTimeSetDone = this.dueTimeSetDone.bind(this)
+    }
 
-constructor(props){
+    dueTimeSetDone() {
+        this.props.hideSideBar();
+    }
 
-super(props);
+    onHandleChange(date) {
+        let id = this.props.id;
+        let todo = this.props.todo;
+        this.setState({
+            startDate: new Date(date)
+        });
 
-this.state = {
+        const putOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ...todo,
+                dueTime: new Date(date)
+            })
+        }
 
-startDate: new Date()
+        this.changeDueDate(id, putOptions, new Date(date));
+    }
 
-};
+    changeDueDate(id, putOptions, dueTime) {
+        window.fetch('http://localhost:3004/todos/' + id, putOptions)
+            .then(res => res.json())
+            .then(json => {
+                this.props.saveDate({
+                    id,
+                    dueTime
+                });
+            });
+    }
 
-this.onHandleChange = this.onHandleChange.bind(this);
-
-this.changeDueDate = this.changeDueDate.bind(this);
-
-this.dueTimeSetDone =this.dueTimeSetDone.bind(this)
-
-}
-
-dueTimeSetDone(){
-
-this.props.hideSideBar();
-
-}
-
-onHandleChange(date){
-
-let id = this.props.id;
-
-let todo = this.props.todo;
-
-this.setState({
-
-startDate: new Date(date)
-
-});
-
-const putOptions = {
-
-method: 'PUT',
-
-headers: {
-
-'Content-Type': 'application/json'
-
-},
-
-body: JSON.stringify({
-
-...todo,
-
-dueTime: new Date(date)
-
-})
-
-}
-
-this.changeDueDate(id,putOptions,new Date(date));
-
-}
-
-changeDueDate(id, putOptions,dueTime) {
-
-window.fetch('http://localhost:3004/todos/' + id, putOptions)
-
-.then(res => res.json())
-
-.then(json => {
-
-this.props.saveDate({id,dueTime});
-
-});
-
-}
-
-render(){
-
-return (
-
-<ReactCSSTransitionGroup
-
-transitionAppear={true}
-
-transitionAppearTimeout={500}
-
-transitionEnterTimeout={400}
-
-transitionLeaveTimeout={800}
-
-transitionName="animated"
-
->
-
-<div key="amache" className="rightSideBar animated fadeInLeftBig" >
-
-<DatePicker selected={this.state.startDate} onChange={this.onHandleChange}/>
-
-<button className="doneButton" onClick={this.dueTimeSetDone}>DONE</button>
-
-</div>
-
-</ReactCSSTransitionGroup>
-
-)
-
-}}
-
+    render() {
+        return (
+            <ReactCSSTransitionGroup
+                transitionAppear={
+                    true
+                }
+                transitionAppearTimeout={
+                    500
+                }
+                transitionEnterTimeout={
+                    400
+                }
+                transitionLeaveTimeout={
+                    800
+                }
+                transitionName="animated"
+            >
+                <div key="amache" className="rightSideBar animated fadeInLeftBig" >
+                    <DatePicker selected={
+                            this.state.startDate
+                        }
+                        onChange={
+                            this.onHandleChange
+                        }
+                    />
+                    <button className = "doneButton"
+                        onClick = {
+                                this.dueTimeSetDone
+                            } > DONE 
+                     </button>
+                 </div>
+            </ReactCSSTransitionGroup> )
+                }
+            }
+            
 export default View;
